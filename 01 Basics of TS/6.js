62c1b8256088463acc18f87b90682e2e99d1f538
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-07-05 00:37:15 +0530

    template string

---
+++ 01 Basics of TS/6.js
@@ -0,0 +1,4 @@
+var nav = "Ram";
+var age = 23;
+console.log("my name is" + nav + "i am " + age + "year old"); // old way
+console.log("My name is ".concat(nav, " i am ").concat(age, " year old")); // template string
---
+++ 01 Basics of TS/6.ts
@@ -0,0 +1,7 @@
+let nav: string = "Ram";
+
+let age: number = 23;
+
+console.log("my name is" + nav + "i am " + age + "year old"); // old way
+
+console.log(`My name is ${nav} i am ${age} year old`); // template string
