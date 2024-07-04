c7ad0bc208cf3a5cd164509288549abc9f7dd08e
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-07-05 00:27:43 +0530

    Usefulstring methods

---
+++ 01 Basics of TS/5.js
@@ -0,0 +1,4 @@
+var myStr = "Hello from India";
+// 1 uppercase
+console.log(myStr.toUpperCase());
+console.log(myStr.toLowerCase());
---
+++ 01 Basics of TS/5.ts
@@ -0,0 +1,7 @@
+let myStr: string = "Hello from India";
+
+// 1 uppercase
+console.log(myStr.toUpperCase());
+
+// 2 lowercase
+console.log(myStr.toLowerCase());
