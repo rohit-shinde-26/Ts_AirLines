fa1141022d381f8c3c30f65d15949fe21a27aac4
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-07-07 12:08:03 +0530

    && and || operator

---
+++ 01 Basics of TS/10.js
@@ -0,0 +1,16 @@
+// && and || operator
+// in && operator if both condition true then true block execute
+var akash = 2;
+var akshay = 32;
+// if (akash && akshay) {
+//   alert("Same Number");
+// } else {
+//   console.warn("Different number");
+// }
+// in || operator if any one condition true then true block execute
+if (akash && akshay) {
+    alert("Same Number");
+}
+else {
+    console.warn("Different number");
+}
---
+++ 01 Basics of TS/10.ts
@@ -0,0 +1,20 @@
+// && and || operator
+
+// in && operator if both condition true then true block execute
+
+let akash: number = 2;
+let akshay: number = 32;
+
+// if (akash && akshay) {
+//   alert("Same Number");
+// } else {
+//   console.warn("Different number");
+// }
+
+// in || operator if any one condition true then true block execute
+
+if (akash && akshay) {
+  alert("Same Number");
+} else {
+  console.warn("Different number");
+}
