1f0294cf46f2777e5e67cf9765fc9a9ea545e732
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-07-07 11:56:51 +0530

    if else statement

---
+++ 01 Basics of TS/8.js
@@ -0,0 +1,8 @@
+// if else statement
+var age = 22;
+if (age > 18) {
+    console.log("You can vote");
+}
+else {
+    console.warn("You cannot vote");
+}
---
+++ 01 Basics of TS/8.ts
@@ -0,0 +1,9 @@
+// if else statement
+
+let age: number = 22;
+
+if (age > 18) {
+  console.log("You can vote");
+} else {
+  console.warn("You cannot vote");
+}
