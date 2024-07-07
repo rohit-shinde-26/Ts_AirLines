e3cfb7bf5bd725f6842c8a9e1e982b16e5314096
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-07-07 12:00:16 +0530

    ternary operator

---
+++ 01 Basics of TS/9.js
@@ -0,0 +1,4 @@
+//  Ternary operator
+var a = 22;
+var vote = a ? "You can vote" : "You cannot vote";
+console.log(vote);
---
+++ 01 Basics of TS/9.ts
@@ -0,0 +1,6 @@
+//  Ternary operator
+
+let a: number = 22;
+
+let vote: string = a ? "You can vote" : "You cannot vote";
+console.log(vote);
