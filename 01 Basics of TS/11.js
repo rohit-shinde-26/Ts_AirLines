a11d742c9cf6734c5fee4353fb4f47f32a041b8a
Rohit <rohitshinde26072003@gmail.com>
2024-07-11 16:42:39 +0530

    Null , undefined, BigInt, typeof

---
+++ 01 Basics of TS/11.js
@@ -0,0 +1,6 @@
+// Null , undefined, BigInt, typeof
+var abhishek = null;
+console.log(typeof abhishek);
+var b = undefined;
+console.log(typeof b);
+var d = "Hello ts";
---
+++ 01 Basics of TS/11.ts
@@ -0,0 +1,11 @@
+// Null , undefined, BigInt, typeof
+
+let abhishek:null = null;
+console.log(typeof abhishek)
+
+let b:undefined = undefined
+console.log(typeof b)
+
+
+
+let d:string = "Hello ts"
