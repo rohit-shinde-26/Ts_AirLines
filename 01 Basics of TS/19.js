89cc0c0cc789214e63a633e038ad3d185a9495a1
Rohit <rohitshinde26072003@gmail.com>
2024-07-12 21:08:21 +0530

    break and continue

---
+++ 01 Basics of TS/19.js
@@ -0,0 +1,14 @@
+// Break and continue 
+var total = 0;
+// for(let i:number = 0 ; i<=10;i++){
+//     if(i===5){
+//         break;
+//     }
+//     console.log(i)
+// }
+for (var i = 0; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
