f50c163f16122910343f0f0989ef8db208781cb9
Rohit <rohitshinde26072003@gmail.com>
2024-07-12 14:44:36 +0530

    switch statement

---
+++ 01 Basics of TS/14.ts
@@ -0,0 +1,17 @@
+// Switch statement
+
+let day:number = 22;
+
+switch(day){
+    case 0:
+        console.log("This is sunday")
+        break;
+    case 1:
+        console.log("This is monday")
+        break;
+    case 2:
+        console.log("This is Tuesday")
+        break;
+    default : 
+    alert("Check the date")
+}
