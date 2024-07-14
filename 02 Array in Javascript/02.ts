262a84ca2f639fb0c1bab9229e9c4bf40261b079
Rohit <rohitshinde26072003@gmail.com>
2024-07-14 10:19:48 +0530

    Push pop shift unshift

---
+++ 02 Array in Javascript/02.ts
@@ -0,0 +1,16 @@
+// Push pop shift unshift
+
+let myCars:String[] = ["Verna N","City","Civic","XUV700"]
+
+// Add element in array at last
+myCars.push("Urus")
+
+// Remove elemet in array from last
+myCars.pop()
+
+// Add element in array at first
+myCars.unshift("Legender")
+
+// Remove first element from array
+myCars.shift()
+console.log(myCars)
