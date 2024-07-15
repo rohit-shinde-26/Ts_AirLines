ccaf8e22b06c8535986d79647fa9f57b0550b025
Rohit <rohitshinde26072003@gmail.com>
2024-07-15 10:34:21 +0530

    spread operator

---
+++ 02 Array in Javascript/03.js
@@ -0,0 +1,13 @@
+// clone array and spread operator 
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var myArr = ["Urus", "XUV 700", "City", "Civic"];
+var newArr = __spreadArray([], myArr, true);
+console.log(newArr);
