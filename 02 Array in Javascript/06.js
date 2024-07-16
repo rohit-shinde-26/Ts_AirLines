18be46177a8e78fb8b19a0b4f5ca5c3a232ab1aa
Rohit <rohitshinde26072003@gmail.com>
2024-07-16 12:41:54 +0530

    for of loop

---
+++ 02 Array in Javascript/06.js
@@ -0,0 +1,8 @@
+// For Of Loop
+var gamingCompo = [];
+var PcCompo = ["Ram", "Monitor", "nvme", "Graphic Card", "Processor"];
+for (var _i = 0, PcCompo_1 = PcCompo; _i < PcCompo_1.length; _i++) {
+    var compo = PcCompo_1[_i];
+    gamingCompo.push(compo);
+}
+console.log(gamingCompo);
---
+++ 02 Array in Javascript/06.ts
@@ -0,0 +1,9 @@
+// For Of Loop
+
+let gamingCompo:string[]= [];
+let PcCompo:string[] = ["Ram","Monitor","nvme","Graphic Card","Processor"]
+
+for(let compo of PcCompo){
+    gamingCompo.push(compo)
+}
+console.log(gamingCompo)
