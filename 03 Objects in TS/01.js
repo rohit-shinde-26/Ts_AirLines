80725ea6c129bbb572bd8f08050ebff3d253fa48
Rohit <rohitshinde26072003@gmail.com>
2024-07-17 16:43:47 +0530

    object intro

---
+++ 03 Objects in TS/01.js
@@ -0,0 +1,13 @@
+var myObj = {
+    name: "Rohit",
+    age: 22,
+    isMale: true
+};
+console.log(myObj);
+// type define 
+var myInfo = {
+    name: "Akanksha",
+    age: 23,
+    isMale: false
+};
+console.log(myInfo);
---
+++ 03 Objects in TS/01.ts
@@ -0,0 +1,19 @@
+// Intro to Objects
+type info = {name:string,age:number,isMale:boolean}  //type define
+const myObj:{name:string,age:number,isMale:boolean} = {
+    name : "Rohit",
+    age : 22 ,
+    isMale: true
+}
+
+console.log(myObj)
+
+// type define 
+
+const myInfo:info = {
+    name:"Akanksha",
+    age: 23,
+    isMale : false
+}
+
+console.log(myInfo)
