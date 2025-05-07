7e97833eb4aa16cbe83aeda8380a5568a3737f6a
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2025-05-07 22:18:43 +0530

    string datatype

---
+++ 02 Type annotion basics/String.js
@@ -0,0 +1,6 @@
+// let myVar: type = value
+var myName = "Ram";
+myName = "Another Name";
+console.log(myName);
+// ERROR
+// myName = 12;
---
+++ 02 Type annotion basics/String.ts
@@ -0,0 +1,8 @@
+// let myVar: type = value
+
+let myName: string = "Ram";
+myName = "Another Name";
+console.log(myName);
+
+// ERROR
+// myName = 12;
