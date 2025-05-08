408a0f6fb15a7c28ea5796d8f5a9d4e47e812529
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2025-05-08 08:53:25 +0530

    return annotions

---
+++ 03 Functions/Return-Annotation.js
@@ -0,0 +1,10 @@
+// Regular Function
+function double1(x) {
+    return x * x;
+}
+var res = double1(2);
+console.log(res);
+// Using Arrow Functions
+var double2 = function (x) { return x * x; };
+var res2 = double2(2);
+console.log(res2);
